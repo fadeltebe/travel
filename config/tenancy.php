f2eff3abaf19534f49e056993078b6d39911a6bd
fadel tebe <auroraexcalibur99@gmail.com>
2026-05-15 11:23:41 +0800

    update env Central Domain

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,10 +17,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
+        '192.168.1.7',
         '127.0.0.1',
         'localhost',
         'travel.test',
-        '192.168.1.7',
     ],
 
     /**
